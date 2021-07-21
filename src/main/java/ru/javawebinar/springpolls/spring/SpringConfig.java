b8b95828f153084d07f012af08b57325215fe6c4
id2k1149 <id2k1149@gmail.com>
2021-07-20 20:43:04 -0400

    v05 delete xml and add SpringConfig java

---
+++ src/main/java/ru/javawebinar/springpolls/spring/SpringConfig.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.springpolls.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import ru.javawebinar.springpolls.model.Answer;
+import ru.javawebinar.springpolls.model.Question;
+import ru.javawebinar.springpolls.model.VotesCounter;
+
+
+@Configuration
+@ComponentScan("ru.javawebinar.springpolls.model")
+@PropertySource("classpath:question.properties")
+public class SpringConfig {
+
+    @Bean
+    public Answer answer() {
+        return new Answer();
+    }
+
+    @Bean
+    public Question question() {
+        return new Question();
+    }
+
+    @Bean
+    public VotesCounter votesCounter() {
+        return new VotesCounter(question(), answer());
+    }
+}
